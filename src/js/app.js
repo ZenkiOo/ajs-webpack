b56bc09922a52eff0b89a31d2d395bdee5b52005
ZenkiOo <zenky@list.ru>
2021-01-10 15:59:20 +0300

    delete second task

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,1 @@
 console.log("app worked");
-
-import Game, {
-  GameSavingData,
-  readGameSaving as loadGame,
-  writeGameSaving as saveGame,
-} from "./game";
-
-const game = new Game();
-game.start();
-loadGame();
